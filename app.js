b3447d704816dc2952c51d6799c3d027be4cfd42
Matheus Sousa <mathsm@br.ibm.com>
2017-07-27 17:13:10 -0300

    Backend fixes

--- app.js
+++ app.js
@@ -2,77 +2,77 @@ let app = require('./config/express.js')();
 let reservationServices = require('./app/services/reservations-services.js');
 let bodyParser = require('body-parser');
 
-function logMe(req,res,next){
-	console.log("touched");
+function logMe(req, res, next) {
+    console.log("touched");
 
-	next();
+    next();
 }
 app.use(bodyParser());
 
 app.get('/reservations', function(req, res) {
-	reservationServices.getList().then( results => {
-		console.log(results);
-		res.send(results);
-	}, error => {
-		res.status(400).send();
-	})
+    reservationServices.getList().then(results => {
+        console.log(results);
+        res.send(results);
+    }, error => {
+        res.status(400).send();
+    })
 });
 
 app.get('/room', function(req, res) {
-	reservationServices.getRoomList().then( results => {
-		console.log(results);
-		res.send(results);
-	}, error => {
-		res.status(400).send();
-	})
+    reservationServices.getRoomList().then(results => {
+        console.log(results);
+        res.send(results);
+    }, error => {
+        res.status(400).send();
+    })
 });
 
 app.get('/room/:id', function(req, res) {
-	reservationServices.getRoomReservationList(req.params.id).then( results => {
-		console.log(results);
-		res.send(results);
-	}, error => {
-		res.status(400).send();
-	})
+    reservationServices.getRoomReservationList(req.params.id).then(results => {
+        console.log(results);
+        res.send(results);
+    }, error => {
+        res.status(400).send();
+    })
 });
 
 app.get('/reservations/:id', function(req, res) {
-	reservationServices.getReservation(req.params.id).then( results => {
-		console.log(results);
-		res.send(results);
-	}, error => {
-		res.status(400).send();
-	})
+    reservationServices.getReservation(req.params.id).then(results => {
+        console.log(results);
+        res.send(results);
+    }, error => {
+        res.status(400).send();
+    })
 });
 
 
 app.post('/reservations', function(req, res) {
-	console.log(req.body);
+    console.log(req.body);
 
-	reservationServices.addReservation(req.body).then( results => {
-		console.log(2);
-		res.send(null)
-	}, error => {
-		console.log(error)
-		res.status(400).send();
-	})
+    reservationServices.addReservation(req.body).then(results => {
+        console.log(2);
+        res.send(null)
+    }, error => {
+        console.log(error)
+        res.status(400).send();
+    })
 });
 
 app.put('/reservations', function(req, res) {
-	reservationServices.editReservation(req.body).then( results => {
-		res.send(null);
-	}, error => {
-		console.log(error)
-		res.status(400).send();
-	}) 
+    reservationServices.editReservation(req.body).then(results => {
+        res.send(null);
+    }, error => {
+        console.log(error)
+        res.status(400).send();
+    })
 });
-app.delete('/reservations', function(req, res) {
-	reservationServices.deleteReservation(req.body).then( results => {
-		res.send(results);
-	}, error => {
-		console.log(error)
-		res.status(400).send();
-	}) 
+app.delete('/reservations/:id', function(req, res) {
+    reservationServices.deleteReservation(req.params.id).then(results => {
+        res.send(results);
+    }, error => {
+        console.log(error)
+        res.status(400).send();
+    })
 });
 
 
--- index/index.js
+++ index/index.js
@@ -1,80 +1,93 @@
 function AppModel() {
-	var self = this;
+    var self = this;
 
-	self.reservationModule = ko.observable(new ReservationModuleModel());
+    self.reservationModule = ko.observable(new ReservationModuleModel());
 }
 
 function ReservationModuleModel() {
-	var self = this;
+    var self = this;
 
-	self.newReservation = ko.observable(new ReservationModel());
-	self.editingReservation = ko.observable(new ReservationModel());
-	self.roomLists = ko.observableArray();
+    self.newReservation = ko.observable(new ReservationModel());
+    self.editingReservation = ko.observable(new ReservationModel());
+    self.roomLists = ko.observableArray();
 }
 
 function ReservationModel() {
-	var self = this;
+    var self = this;
 
-	self.reservationId = ko.observable(null);
-	self.reservationInitHour = ko.observable(null);
-	self.reservationEndHour = ko.observable(null);
-	self.reservationRoom = ko.observable(new RoomModel);
-	self.reservationRequester = ko.observableArray();
-	self.reservationTimeRange = ko.computed(function () {
-		return self.reservationEndHour() - self.reservationInitHour();
-	})
+    self.reservationId = ko.observable(null);
+    self.reservationInitHour = ko.observable(null);
+    self.reservationEndHour = ko.observable(null);
+    self.reservationRoom = ko.observable(new RoomModel);
+    self.reservationRequester = ko.observableArray();
+    self.reservationTimeRange = ko.computed(function() {
+        return self.reservationEndHour() - self.reservationInitHour();
+    })
 }
 
 function RoomModel() {
-	var self = this;
+    var self = this;
 
-	self.roomName = ko.observableArray();
-	self.roomId = ko.observableArray();
-	self.roomReservationList = ko.observableArray();
+    self.roomName = ko.observableArray();
+    self.roomId = ko.observableArray();
+    self.roomReservationList = ko.observableArray();
 }
 
 function loadRoomList() {
-	$.ajax({
-		url: '/room',
-		type: 'get',
-		data: data,
-		dataType: 'json',
-		success: function (data) {
-			
-			ko.utils.arrayForEach(data, function (room) {
-				if (self.roomList.indexOf(room.roomId) === -1) {
-					room = new RoomModel();
-					self.roomList.push(room);
-				}
-			});
-		}
-	});
+    $.ajax({
+        url: '/room',
+        type: 'get',
+        data: data,
+        dataType: 'json',
+        success: function(data) {
+
+            ko.utils.arrayForEach(data, function(room) {
+                if (self.roomList.indexOf(room.roomId) === -1) {
+                    room = new RoomModel();
+                    self.roomList.push(room);
+                }
+            });
+        }
+    });
 }
 
-function loadReservationLists() {
-	$.ajax({
-		url: '/reservation',
-		type: 'get',
-		data: data,
-		dataType: 'json',
-		success: function (data) {
-			ko.utils.arrayForEach(data, function (reservation) {
-				
-			});
-		}
-	});
+function loadRoomReservationList(room) {
+    $.ajax({
+        url: '/reservation/' + room.roomId,
+        type: 'get',
+        data: data,
+        dataType: 'json',
+        success: function(data) {
+            room.roomReservationList.push(data);
+        }
+    });
 }
 
 function addReservation() {
-	self.dayReservations.push(self.dayReservation());
+    self.dayReservations.push(self.dayReservation());
 
 }
-function removeReservation() {
-	//remover a reserva da array
+
+function removeReservation(reservationId) {
+    $.ajax({
+        url: '/reservation/' + reservationId,
+        type: 'delete',
+        success: function(data) {
+
+        },
+        fail: function() {
+            console.log('Deletion failed')
+        }
+    });
 }
+
 function editReservation() {
-	//editar a reserva
+    //editar a reserva
 }
+
+ko.utils.arrayForEach(self.roomList, function(room) {
+    loadRoomReservationList(room);
+})
 
 var app = new AppModel();
 ko.applyBindings(app);
